227d6b8b84ace225730a90bb7c00690e2744c34a
mihad-khadem <mihadkhadem@gmail.com>
2024-05-16 00:44:02 +0600

    interfaces/types added

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,18 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import config from "./app/config";
+
 // Application
 const app: Application = express();
+
 // parsers
 app.use(express.json());
 app.use(cors());
 
 app.get("/", (req: Request, res: Response) => {
   var a = 10;
-  res.send(a);
+  // Send a JSON response with a property named "data" containing the value of 'a'
+  res.json({ data: a });
 });
 
 export default app;
